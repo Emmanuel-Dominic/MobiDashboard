bf751f113f5563c2b39078a0095b5cdc1e1d037e
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2024-09-07 10:57:39 +0300

    add students list

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,58 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  studentList = students;
   constructor() {}
 
 }
+
+const students = [
+  {
+    id: 1,
+    name: "Emmanuel"
+  },
+  {
+    id: 2,
+    name: "Dominic"
+  },
+  {
+    id: 3,
+    name: "Matembu"
+  },
+  {
+    id: 4,
+    name: "Shalom"
+  },
+  {
+    id: 5,
+    name: "Darren"
+  },
+  {
+    id: 6,
+    name: "Makeila"
+  },
+  {
+    id: 7,
+    name: "Pamela"
+  },
+  {
+    id: 8,
+    name: "Yivan"
+  },
+  {
+    id: 9,
+    name: "Akiliboss"
+  },
+  {
+    id: 10,
+    name: "Mutoni"
+  },
+  {
+    id: 11,
+    name: "Kirabo"
+  },
+  {
+    id: 12,
+    name: "Evelyn"
+  }
+]
